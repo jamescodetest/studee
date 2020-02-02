be87c67ed7eef71f5bfda4e7846d5348d7a86482
James Tookey <jamestooks@gmail.com>
2020-02-02 14:01:44 +0000

    SQL query altered based on database changes

--- Application/Services/CountryService.php
+++ Application/Services/CountryService.php
@@ -23,12 +23,21 @@ class CountryService
 
     public function searchCountryDatabase(\Application\Models\CountrySearchModel $_countrySearchModel): array
     {
-        $results = [];
-
-        $sql = 'SELECT *
-                FROM countries';
+        $sql = 'SELECT c.*, currencies.currency_codes, timezones.timezones
+                FROM countries AS c
+                LEFT JOIN (
+                    SELECT country_id, GROUP_CONCAT(DISTINCT currency_code ORDER BY currency_code ASC SEPARATOR ", ") AS currency_codes
+                    FROM countries_currencies
+                    GROUP BY country_id
+                ) AS currencies ON c.id = currencies.country_id
+                LEFT JOIN (
+                    SELECT country_id, GROUP_CONCAT(DISTINCT timezone ORDER BY timezone ASC SEPARATOR ", ") AS timezones
+                    FROM countries_timezones
+                    GROUP BY country_id
+                ) AS timezones ON c.id = timezones.country_id';
 
         $where = [];
+        $having = [];
         $params = [];
 
         if ($_countrySearchModel->countryName != '') {
@@ -46,21 +55,24 @@ class CountryService
             $params['capital_city'] = '%' . $_countrySearchModel->capitalCity . '%';
         }
 
-        if ($_countrySearchModel->currency_code != '') {
-            $where[] = 'currency_code LIKE :currency_code';
-            $params['currency_code'] = '%' . $_countrySearchModel->currency_code . '%';
+        if ($_countrySearchModel->currencyCode != '') {
+            $having[] = 'currencies.currency_codes LIKE :currency_code';
+            $params['currency_code'] = '%' . $_countrySearchModel->currencyCode . '%';
         }
 
         if ($_countrySearchModel->language != '') {
-            $where[] = 'language LIKE :language';
+            $where[] = 'primary_language LIKE :language';
             $params['language'] = '%' . $_countrySearchModel->language . '%';
         }
 
         if (count($where) > 0) {
             $sql .= sprintf(' WHERE %s', implode(' AND ', $where));
-            $results = $this->dbConn->getAll($sql, $params);
         }
 
-        return $results;
+        if (count($having) > 0) {
+            $sql .= sprintf(' HAVING %s', implode(' AND ', $having));
+        }
+
+        return $this->dbConn->getAll($sql, $params);;
     }
 }
